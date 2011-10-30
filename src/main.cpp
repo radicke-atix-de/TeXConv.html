a4b4708a4452c79c75810c1de63b0f45a6a56dcf
Olaf Radicke <briefkasten@olaf-radicke.de>
2011-10-30 11:39:01 +0100

    try ried an wire files.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 // http://laurel.datsi.fi.upm.es/~rpons/gjsc/doxygen/classyyFlexLexer.html
 // http://dinosaur.compilertools.net/flex/flex_19.html
 
+// TODO: create subfunctions for open and close file.
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,7 +18,6 @@
 
 
 
-
 main(int argc,char *argv[])
 {
     std::string filename_in;
@@ -24,7 +25,7 @@ main(int argc,char *argv[])
     std::string filename_out;
     filename_out = "";
     std::string filename_tmp;
-    filename_tmp = "";
+    filename_tmp = "texconv_tex.tmp";
     
     if(argc!=3)
     {
@@ -33,52 +34,64 @@ main(int argc,char *argv[])
     }
     filename_in = argv[1];
     filename_out = argv[2];
-    filename_tmp = filename_out + std::string(".tmp");
 
+    
+    // open fiele to write....
+    std::ofstream tmp_file (filename_tmp.c_str());
+    if (tmp_file.is_open())
+    {
+//         tmp_file << "This is a line.\n";
+//         tmp_file << "This is another line.\n";
+    }else{
+        std::cout << "Unable to open file" << std::endl;
+        return 0;
+    }    
 
     // open fiele to read....
-    std::ifstream read_file(filename_in.c_str());
-    if (read_file.is_open())
+    std::ifstream tex_file(filename_in.c_str());
+    if (tex_file.is_open())
     {
-//         while ( read_file.good() )
-//         {
-//             std::string line;
-//             getline (read_file,line);
-//             std::cout << line << std::endl;
-//         }
+        while ( tex_file.good() )
+        {
+            std::string line;
+            getline (tex_file,line);
+            tmp_filet << line << std::endl;
+        }
     }else{
         std::cout << "couldn't open temp for writting\n" << std::endl;
         return 0;
     }
 
+
+
     // open fiele to write....
-    std::ofstream write_file_tmp (filename_tmp.c_str());
-    if (write_file_tmp.is_open())
+    std::ofstream err_file ("error.log");
+    if (err_file.is_open())
     {
-//         write_file_tmp << "This is a line.\n";
-//         write_file_tmp << "This is another line.\n";
+//         tmp_file << "This is a line.\n";
+//         tmp_file << "This is another line.\n";
     }else{
         std::cout << "Unable to open file" << std::endl;
         return 0;
-    }
-
-    
-    newcomFlexLexer comLexer; //( &read_file,  &write_file_tmp);
-    comLexer.switch_streams( &read_file,  &write_file_tmp);
+    }  
+  
+  
+    newcomFlexLexer comLexer; //( &tex_file,  &tmp_file);
+    comLexer.switch_streams( &tex_file,  &err_file);
     comLexer.yylex();
-    read_file.close();
-    write_file_tmp.close();
+//     tex_file.close();
+    tmp_file.close();
 
     // Main processing.
 
     // open file to read...
-    std::ifstream read_file_tmp(filename_tmp.c_str());
-    if (read_file_tmp.is_open())
+    std::ifstream tex_file_tmp(filename_tmp.c_str());
+    if (tex_file_tmp.is_open())
     {
-//         while ( read_file_tmp.good() )
+//         while ( tex_file_tmp.good() )
 //         {
 //             std::string line;
-//             getline (read_file_tmp,line);
+//             getline (tex_file_tmp,line);
 //             std::cout << line << std::endl;
 //         }
     }else{
@@ -86,36 +99,32 @@ main(int argc,char *argv[])
         return 0;
     }
 
+
+
+//     FlexLexer htmlLexer( &tex_file_tmp,  &html_file);
+//     htmlFlexLexer htmlLexer( &tex_file_tmp,  &err_file);
+    htmlFlexLexer htmlLexer( &tex_file_tmp,  &err_file);
+    htmlLexer.yylex();
+    
     // file open for writeing
-    std::ofstream write_file (filename_out.c_str());
-    if (write_file.is_open())
+    std::ofstream html_file (filename_out.c_str());
+    if (html_file.is_open())
     {
-//         write_file << "This is a line.\n";
-//         write_file << "This is another line.\n";
+//         html_file << "This is a line.\n";
+//         html_file << "This is another line.\n";
     }else{
         std::cout << "Unable to open file" << std::endl;
         return 0;
-    }
-
-//     FlexLexer htmlLexer( &read_file_tmp,  &write_file);
-    htmlFlexLexer htmlLexer( &read_file_tmp,  &write_file);
-    htmlLexer.yylex();
-    read_file_tmp.close();
-    write_file.close();
+    }    
+    
+    tex_file_tmp.close();
+    html_file.close();
 
 
     std::cout << "tmp-fiel-name: " << filename_tmp << std::endl;
     std::cout << "filename_out: " << filename_out << std::endl;
+//    std::cout << htmlLexer.html_open_round_brackets << std::endl;
     return 0;
 }
 
 
-// int main()
-// {
-//     yyFlexLexer Lexer;
-//     Lexer.yylex();
-// 
-//     std::cout << "Bilanz (: " << open_round_brackets << std::endl;
-//     std::cout << "Bilanz [: " << open_square_brackets << std::endl;
-//     std::cout << "Bilanz {: " << open_curly_brackets << std::endl;
-// }
